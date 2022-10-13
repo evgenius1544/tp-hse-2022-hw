c3b6bb7fc522e2298393500337a55f60ebd8423d
evgenius1544 <shpakova1201@mail.ru>
2022-10-14 00:46:57 +0300

    v0.1

---
+++ app/main.py
@@ -0,0 +1,129 @@
+"""
+Implement in Python a simple program that will read numbers from a file,
+and then use separate functions to search among these numbers for the minimum number,
+the maximum number, count their total sum and product.
+"""
+
+import sys
+import os
+
+from functools import reduce
+
+from typing import Union
+from typing import List
+from typing_extensions import TypeAlias
+
+
+Number: TypeAlias = Union[int, float]
+
+
+def _min(values: List[Number]) -> Number:
+    """
+    _min
+        Finds min element in sequence.
+
+    Args:
+        values (List[Number]): values.
+
+    Returns:
+        Number: minimum.
+    """
+
+    return min(values)
+
+
+def _max(values: List[Number]) -> Number:
+    """
+    _max
+        Finds maxx element in sequence.
+
+    Args:
+        values (List[Number]): values.
+
+    Returns:
+        Number: maximum.
+    """
+
+    return max(values)
+
+
+def _sum(values: List[Number]) -> Number:
+    """
+    _sum
+        Function sums new value with previous result.
+
+    Args:
+        values (List[Number]): values.
+
+    Returns:
+        Number: result of sum.
+    """
+
+    return sum(values)
+
+
+def _mult(values: List[Number]) -> Number:
+    """
+    _mult
+        Function multiples new value with previous result.
+
+    Args:
+        values (List[Number]): values.
+
+    Returns:
+        Number: result of mult.
+    """
+
+    return reduce(lambda x, y: x * y, values)
+
+
+def _read(file_path: str) -> List[Number]:
+    """
+    _read
+        Reads all values from file.
+
+
+    Returns:
+        List[Number]: values in list.
+    """
+
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        return [float(el) for el in file.readline().split()]
+
+
+def _output(min_: Number, max_: Number, sum_: Number, mult_: Number) -> None:
+    """
+    _output
+        Outputs data.
+    """
+
+    print(min_, max_, sum_, mult_)
+
+
+def main(file_name: str, output: bool = False) -> None:
+    """
+    main
+        Reads values and processes them.
+    """
+
+    if not os.path.exists(file_name):
+        exit("Args error: File not found ...")
+
+    file_path = os.path.abspath(file_name)
+
+    values = _read(file_path)
+
+    minimum = _min(values)
+    maximum = _max(values)
+    summa = _sum(values)
+    multiplication = _mult(values)
+
+    if output:
+        _output(minimum, maximum, summa, multiplication)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        exit("Args error: Waiting your file ...")
+
+    main(sys.argv[1], output=True)
---
+++ app/test.py
@@ -0,0 +1,74 @@
+"""
+All needed tests for sum, mult, min, max,
+where values are equivalent, monotonic and two types.
+"""
+
+import unittest
+import main as m
+
+
+class MinimumTest(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(m._min([i for i in range(1, 10)]) == 1)
+
+    def test_2(self):
+        self.assertTrue(m._min([i for i in range(-10, 1)]) == -10)
+
+    def test_3(self):
+        self.assertTrue(m._min([0, 0, 0, 0]) == 0)
+
+    def test_4(self):
+        self.assertTrue(m._min([1, -1, 1, -1, 1, -1]) == -1)
+
+
+class MaximumTest(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(m._max([i for i in range(1, 10)]) == 9)
+
+    def test_2(self):
+        self.assertTrue(m._max([i for i in range(-10, 1)]) == 0)
+
+    def test_3(self):
+        self.assertTrue(m._max([0, 0, 0, 0]) == 0)
+
+    def test_4(self):
+        self.assertTrue(m._max([1, -1, 1, -1, 1, -1]) == 1)
+
+
+class SummaTest(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(m._sum([i for i in range(1, 10)]) == 45)
+
+    def test_2(self):
+        self.assertTrue(m._sum([i for i in range(-10, 1)]) == -55)
+
+    def test_3(self):
+        self.assertTrue(m._sum([0, 0, 0, 0]) == 0)
+
+    def test_4(self):
+        self.assertTrue(m._sum([1, -1, 1, -1, 1, -1]) == 0)
+
+
+class MultiplicationTest(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(m._mult([i for i in range(1, 10)]) == 362880)
+
+    def test_2(self):
+        self.assertTrue(m._mult([i for i in range(-10, 1)]) == 0)
+
+    def test_3(self):
+        self.assertTrue(m._mult([0, 0, 0, 0]) == 0)
+
+    def test_4(self):
+        self.assertTrue(m._mult([1, -1, 1, -1, 1, -1]) == -1)
+
+
+class ReadTest(unittest.TestCase):
+    def test(self):
+        self.assertTrue(
+            m._read("tests/test_for_read.txt") == [1, 2, 3, 4, 0, 0, -1, -1]
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
